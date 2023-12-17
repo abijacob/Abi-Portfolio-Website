ea480d2f3d1bbe0a591c29dc13387abbd01f4f21
Abi Jacob <abijacob92@gmail.com>
2023-12-17 11:19:32 -0800

    Basic Layout and assets updated

---
+++ src/components/header/headerSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const headerSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://www.linkedin.com/in/abi-jacob/' target='_blank'></a>
+        <a href='https://github.com/abijacob' target='_blank'></a>
+    </div>
+  )
+}
+
+export default headerSocials
