9d1c2b6950f47b7e14b6c1a9bc3dc7530d318ce7
Abi Jacob <abijacob92@gmail.com>
2023-12-19 22:38:07 -0800

    Added Nav Section and Floating Nav Functionality

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './nav.css'
+import { FaHome } from "react-icons/fa";
+import { FcAbout } from "react-icons/fc";
+import { FaBookOpen } from "react-icons/fa6";
+import { RiServiceLine } from "react-icons/ri";
+import { MdContacts } from "react-icons/md";
+import { useState } from 'react';
 
-const nav = () => {
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>nav</div>
+    <nav>
+      <a href='#' className={activeNav === '#' ? 'active' : ''}><FaHome /></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FcAbout /></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaBookOpen /></a>
+      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdContacts /></a>
+    </nav>
   )
 }
 
-export default nav
+export default Nav
